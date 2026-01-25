8def52e369ea39e48207e8290350683c2fd12845
lkassaei <lily.kassaei@menloschool.org>
2026-01-25 11:31:40 -0800

    v1

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -3,7 +3,7 @@
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author Lily Kassaei
  */
 
 public class MakingChange {
@@ -11,7 +11,23 @@ public class MakingChange {
      * TODO: Complete this function, countWays(), to return the number of ways to make change
      *  for any given total with any given set of coins.
      */
+    public static long ways;
     public static long countWays(int target, int[] coins) {
-        return 0;
+        ways = 0;
+        int count = 0;
+        countWaysHelper(target, count, coins);
+        return ways;
+    }
+
+    public static void countWaysHelper(int target, int count, int[] coins) {
+        if (count == target) {
+            ways++;
+        }
+        if (count > target) {
+            return;
+        }
+        for (int i = 0; i < coins.length; i++) {
+            countWaysHelper(target, count + coins[i], coins);
+        }
     }
 }
