e43d688dd74c07be13ec686d62a042052be09c94
lkassaei <lily.kassaei@menloschool.org>
2026-01-29 20:16:14 -0800

    v2 progress

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,27 +7,49 @@
  */
 
 public class MakingChange {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
-    public static long ways;
+    // Array to store passed calculations using memoization.
+    public static long[][] dp;
+
+    // Given a target and array of coin values, return how many ways can you make target with coins
     public static long countWays(int target, int[] coins) {
-        ways = 0;
-        int count = 0;
-        countWaysHelper(target, count, coins);
-        return ways;
+        // Set the size of dp to target + 1 in the cols to include 0-target
+        dp = new long[coins.length][target + 1];
+
+        // Go through coins and target, filling in the array with memoization
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 0; j < target + 1; j++) {
+                // Fill in array passing in the coinIndex, coins array, and target
+                dp[i][j] = countWaysHelper(i, coins, j);
+            }
+        }
+        // Return the bottom right square
+        return dp[coins.length - 1][target];
     }
 
-    public static void countWaysHelper(int target, int count, int[] coins) {
-        if (count == target) {
-            ways++;
+    // Memoization
+    public static long countWaysHelper(int coinIndex, int[] coins, int target) {
+        // Base cases:
+        // If there are no more coins to use then return 0
+        if (coinIndex < 0 || coins.length == 0) {
+            return 0;
         }
-        if (count > target) {
-            return;
+        // If we made it to 0 then we have found 1 way
+        if (target == 0) {
+            return 1;
         }
-        for (int i = 0; i < coins.length; i++) {
-            countWaysHelper(target, count + coins[i], coins);
+        // If there is one coin left and if can't make target than return 0
+        if (coinIndex == 0 && target % coins[coinIndex] != 0) {
+            return 0;
         }
+        // If target is negative then return 0
+        if (target < 0) {
+            return 0;
+        }
+        // If we already have calculated then return that number
+        if (dp[coinIndex][target] != 0) {
+            return dp[coinIndex][target];
+        }
+        // Return the sum of the two ways: including the current coin or excluding
+        return countWaysHelper(coinIndex - 1, coins, target) + countWaysHelper(coinIndex, coins, target - coins[coinIndex]);
     }
 }
